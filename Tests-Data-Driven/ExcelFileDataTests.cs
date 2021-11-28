efdcc5b530a0233444ce23503a4b1bcb1964abbe
Adelina.Petrova <adelina.petrova@softwaregroup.com>
2021-11-28 11:20:58 +0200

    Add project files.

---
+++ Tests-Data-Driven/ExcelFileDataTests.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using RestSharp;
+using RestSharp.Serialization.Json;
+using SpreadsheetLight;
+using System.Collections.Generic;
+
+namespace Tests_Data_Driven
+{
+    class ExcelFileDataTests
+    {
+        [TestCaseSource("LoadTestDataFromExcel")]
+        public void TestZippopotamous(string countryCode, string zipCode, string expectedPlace, string expectedStateCode)
+        {
+            //Arrange
+            var restClient = new RestClient("https://api.zippopotam.us");
+            var httpRequest = new RestRequest(countryCode + "/" + zipCode);
+
+            //Act
+            var httpResponse = restClient.Execute(httpRequest);
+            var location = new JsonDeserializer().Deserialize<Location>(httpResponse);
+
+            //Assert
+            StringAssert.Contains(expectedPlace, location.Places[0].PlaceName);
+            StringAssert.Contains(expectedStateCode, location.Places[0].StateAbbreviation);
+        }
+        static IEnumerable<TestCaseData> LoadTestDataFromExcel()
+        {
+            using (var sheet = new SLDocument("../../../ZippopotamousTestData.xlsx"))
+            {
+                int endRowIndex = sheet.GetWorksheetStatistics().EndRowIndex;
+                for (int row = 2; row <= endRowIndex; row++)
+                {
+                    string countryCode = sheet.GetCellValueAsString(row, 1);
+                    string zipCode = sheet.GetCellValueAsString(row, 2);
+                    string expectedPlace = sheet.GetCellValueAsString(row, 3);
+                    string expectedStateCode = sheet.GetCellValueAsString(row, 4);
+                    yield return new TestCaseData(countryCode, zipCode, expectedPlace, expectedStateCode);
+                }
+            }
+            //yield return new TestCaseData("BG", "1000", "Sofija");
+            //yield return new TestCaseData("BG", "5000", "Veliko Turnovo");
+            //yield return new TestCaseData("CA", "M5S", "Toronto");
+            //yield return new TestCaseData("GB", "B1", "Birmingham");
+            //yield return new TestCaseData("DE", "01067", "Dresden");
+        }
+    }
+}
---
+++ Tests-Data-Driven/SeleniumDataDrivenTests.cs
@@ -0,0 +1,133 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+
+namespace Tests_Data_Driven
+{
+    class SeleniumDataDrivenTests
+    {
+        RemoteWebDriver driver;
+        IWebElement textboxNum1;
+        IWebElement textboxNum2;
+        IWebElement dropdownOptions;
+        IWebElement buttonCalculate;
+        IWebElement buttonReset;
+        IWebElement result;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://number-calculator.nakov.repl.co/");
+
+            textboxNum1 = driver.FindElementById("number1");
+            dropdownOptions = driver.FindElementById("operation");
+            textboxNum2 = driver.FindElementById("number2");
+            result = driver.FindElementById("result");
+            buttonCalculate = driver.FindElementById("calcButton");
+            buttonReset = driver.FindElementById("resetButton");
+
+
+        }
+
+        //Test with valid integers
+        [TestCase("5", "+", "3", "Result: 8")]
+        [TestCase("5", "*", "3", "Result: 15")]
+        [TestCase("12", "/", "3", "Result: 4")]
+        [TestCase("5", "-", "3", "Result: 2")]
+
+        //Test with valid decimals
+        [TestCase("5.23", "+", "3.88", "Result: 9.11")]
+        [TestCase("3.14", "-", "12.763", "Result: -9.623")]
+        [TestCase("3.14", "*", "-7.534", "Result: -23.65676")]
+        [TestCase("12.5", "/", "4", "Result: 3.125")]
+
+
+        //Test with invalid input
+        [TestCase("", "+", "3", "Result: invalid input")]
+        [TestCase("5", "+", "", "Result: invalid input")]
+        [TestCase("asass", "+", "3", "Result: invalid input")]
+        [TestCase("", "+", "", "Result: invalid input")]
+        [TestCase("asa", "+", "sasa", "Result: invalid input")]
+        [TestCase("4", "+", "sasa", "Result: invalid input")]
+
+        [TestCase("", "-", "3", "Result: invalid input")]
+        [TestCase("5", "-", "", "Result: invalid input")]
+        [TestCase("asass", "-", "3", "Result: invalid input")]
+        [TestCase("", "-", "", "Result: invalid input")]
+        [TestCase("asa", "-", "sasa", "Result: invalid input")]
+        [TestCase("4", "-", "sasa", "Result: invalid input")]
+
+        [TestCase("", "*", "3", "Result: invalid input")]
+        [TestCase("5", "*", "", "Result: invalid input")]
+        [TestCase("asass", "*", "3", "Result: invalid input")]
+        [TestCase("", "*", "", "Result: invalid input")]
+        [TestCase("asa", "*", "sasa", "Result: invalid input")]
+        [TestCase("4", "*", "sasa", "Result: invalid input")]
+
+        [TestCase("", "/", "3", "Result: invalid input")]
+        [TestCase("5", "/", "", "Result: invalid input")]
+        [TestCase("asass", "/", "3", "Result: invalid input")]
+        [TestCase("", "/", "", "Result: invalid input")]
+        [TestCase("asa", "/", "sasa", "Result: invalid input")]
+        [TestCase("4", "/", "sasa", "Result: invalid input")]
+
+        //Invalid operations
+        [TestCase("4", "@", "7", "Result: invalid operation")]
+        [TestCase("4", "!!!!", "7", "Result: invalid operation")]
+        [TestCase("4", "", "7", "Result: invalid operation")]
+
+        //Test with Infinity
+        [TestCase("Infinity", "+", "1", "Result: Infinity")]
+        [TestCase("Infinity", "-", "1", "Result: Infinity")]
+        [TestCase("Infinity", "*", "1", "Result: Infinity")]
+        [TestCase("Infinity", "/", "1", "Result: Infinity")]
+        [TestCase("1", "+", "Infinity", "Result: Infinity")]
+        [TestCase("1", "-", "Infinity", "Result: -Infinity")]
+        [TestCase("1", "*", "Infinity", "Result: Infinity")]
+        [TestCase("1", "/", "Infinity", "Result: 0")]
+        [TestCase("Infinity", "*", "Infinity", "Result: Infinity")]
+        [TestCase("Infinity", "+", "Infinity", "Result: Infinity")]
+
+        //Test with invalid calculation
+        [TestCase("Infinity", "/", "Infinity", "Result: invalid calculation")]
+        [TestCase("Infinity", "-", "Infinity", "Result: invalid calculation")]
+
+        //Test with exponential numbers
+        [TestCase("1.5e53", "*", "150", "Result: 2.25e+55")]
+        [TestCase("1.5e53", "/", "150", "Result: 1e+51")]
+
+        public void TestCalculatorWebApp(string num1, string op, string num2, string expectedResult)
+        {
+            //Arrange
+            buttonReset.Click();
+            if (num1 != "")
+            {
+                textboxNum1.SendKeys(num1);
+            }
+            if (op != "")
+            {
+                dropdownOptions.SendKeys(op);
+            }
+            if (num2 != "")
+            {
+                textboxNum2.SendKeys(num2);
+            }
+
+            //Act
+            buttonCalculate.Click();
+
+            //Assert
+            var actualResult = result.Text;
+            Assert.AreEqual(expectedResult, actualResult);
+            
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
